ff38072bbd2c26301d5a8669858a1dc2baa12670
임성우 <seongwoorang@naver.com>
2024-11-09 15:38:52 +0900

    [feature-login] - Add navigation module

---
+++ features/feature-navigation/src/main/java/com/example/feature_navigation/Route.kt
@@ -0,0 +1,9 @@
+package com.example.feature_navigation
+
+object Route {
+    const val HOME = "HOME"
+    const val GRAPH = "GRAPH"
+    const val SETTINGS = "SETTINGS"
+}
+
+val showBarList = listOf(Route.HOME)
